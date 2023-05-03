b78bd2eec522806e572a05278b3f60906a295f41
carlos <carlosleret@gmail.com>
2023-05-03 19:49:50 +0200

    add mocked data for test

--- typescript-react/src/components/ListingCard/ListingCard.spec.tsx
+++ typescript-react/src/components/ListingCard/ListingCard.spec.tsx
@@ -2,8 +2,28 @@ import { render } from '@testing-library/react';
 
 import ListingCard from './ListingCard';
 
+const mockedEntry = {
+  bedrooms_count: 2,
+  building_type: 'STUDIO',
+  contact_phone_number: '+219779210354',
+  created_date: '2023-01-17T14:19:22.808738',
+  description: '',
+  id: 1,
+  latest_price_eur: 710000.0,
+  name: 'Mikhail Schmiedt',
+  postal_address: {
+    city: 'Berchtesgaden',
+    country: 'DE',
+    postal_code: '21810',
+    street_address: 'Johan-Ernst-Ring 7',
+  },
+  rooms_count: 6,
+  surface_area_m2: 167.0,
+  updated_date: '2023-01-17T14:20:47.707666',
+};
+
 describe('<ListingCard /> test suite', () => {
   it('Should render the <ListingCard /> component', () => {
-    render(<ListingCard />);
+    render(<ListingCard {...mockedEntry} />);
   });
 });
