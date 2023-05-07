ffb481fdcd3f15300c6d520a1fb6475d7cae601f
carlos <carlosleret@gmail.com>
2023-05-07 15:41:56 +0200

    add tests for Header component

--- typescript-react/src/components/Header/Header.spec.tsx
+++ typescript-react/src/components/Header/Header.spec.tsx
@@ -1,9 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Header from '.';
 
 describe('<Header />', () => {
   it('Should render the header component', () => {
     render(<Header />);
+  });
+  it('renders a logo', () => {
+    render(<Header />);
+    const imageAviv = screen.getByRole('img');
+    expect(imageAviv).toBeInTheDocument();
+  });
+
+  it('it takes full width', () => {
+    render(<Header />);
+    const headerElement = screen.getByRole('banner');
+    const headerStyle = window.getComputedStyle(headerElement);
+    expect(headerStyle.display).toBe('block');
   });
 });
