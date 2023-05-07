f7e48446f007ca84ae58bad4d0c91a6fd90faf85
carlos <carlosleret@gmail.com>
2023-05-07 16:26:12 +0200

    add basic tests to form

--- typescript-react/src/components/ListingForm/ListingForm.spec.tsx
+++ typescript-react/src/components/ListingForm/ListingForm.spec.tsx
@@ -1,9 +1,19 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import ListingForm from '.';
 
 describe('<ListingForm />', () => {
   it('Should render the listing form component', () => {
     render(<ListingForm />);
+  });
+
+  it('form fields exist', () => {
+    render(<ListingForm />);
+    const nameInput = screen.getByRole('button', { name: /Create/i });
+    const optionInput = screen.getByRole('option', {
+      name: /STUDIO/i,
+    });
+    expect(nameInput).toBeInTheDocument();
+    expect(optionInput).toBeInTheDocument();
   });
 });
